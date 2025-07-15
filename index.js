4a63c137b285b39993dd3b3937e2e72be422fa45
GJoelO <gastonleppen@gmail.com>
2025-07-15 12:49:29 -0300

    hubo cambios en la conexion DB

--- index.js
+++ index.js
@@ -115,6 +115,5 @@
     })
     })
 
-    mysql://root:NRidEGmvzsDOxKmBzPiogDWoWuGTLwsp@yamabiko.proxy.rlwy.net:14027/railway
     const PORT = process.env.PORT || 3088
     app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
